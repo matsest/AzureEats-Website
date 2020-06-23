be8c9d3347d46b292d2ba0873cfe7e5ae58efec0
Mats Estensen <mats.est@gmail.com>
2020-06-23 23:14:23 +0200

    unique name for app

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "azurerm_app_service_plan" "website" {
 }
 
 resource "azurerm_app_service" "website" {
-  name                = var.app_service_name
+  name                = "${var.app_service_name}-with-db"
   location            = azurerm_resource_group.app.location
   resource_group_name = azurerm_resource_group.app.name
   app_service_plan_id = azurerm_app_service_plan.website.id
